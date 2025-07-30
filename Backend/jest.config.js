5478dbe48e85a1aff4b4a3c42e1ca812b9b1102a
Hari Haran Rathinakumar <24024414@studentmail.ul.ie>
2025-07-30 12:42:18 +0100

    feat(NIP-13): Testcase

--- Backend/jest.config.js
+++ Backend/jest.config.js
@@ -6,8 +6,17 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.ts'], // test files outside src/
+  testMatch: ['**/tests/**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js', 'json'],
   moduleDirectories: ['node_modules', 'src'],
-  rootDir: '.', // root is project root
+  rootDir: '.',
+
+  // 👇 Add these lines
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: './test-results',
+      outputName: 'test-report.xml',
+    }],
+  ],
 };
